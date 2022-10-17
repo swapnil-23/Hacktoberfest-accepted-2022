4343cc26245d6a3b9de1a6bfae55c043029eb259
SWAPNIL BISWAS <73814033+swapnil-23@users.noreply.github.com>
2022-10-18 01:04:23 +0530

    Function commited

---
+++ src/Function.java
@@ -0,0 +1,14 @@
+public class Function {
+    public static void main(String [] args)
+    {
+        int ans = sum(10, 20);
+        System.out.println(ans);
+    }
+
+    static int sum(int a, int b)
+    {
+        int num1, num2;
+        int sum = a+b;
+        return sum;
+    }
+}
