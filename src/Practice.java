74ab85368e1e9aa94e5e15663c99791daab3c64c
SWAPNIL BISWAS <73814033+swapnil-23@users.noreply.github.com>
2022-10-18 01:05:03 +0530

    Function commited

--- src/Practice.java
+++ src/Practice.java
@@ -1,30 +1,19 @@
 import java.util.Scanner;
 
+import java.math.*;
+
 public class Practice {
     public static void main(String[] args)
     {
+        Circumference();
+    }
+
+    static void Circumference()
+    {
         Scanner in = new Scanner(System.in);
-        System.out.println("enter to check the number is palindrome or not: ");
-        int num = in.nextInt();
+        int rad = in.nextInt();
 
-        int reversum = 0, remainder;
-
-        int originalnum = num;
-
-        while(num!=0)
-        {
-            remainder = num%10;
-            reversum = reversum*10+remainder;
-            num /= 10;
-        }
-
-        // to check if the number is palindrome or not
-        if(reversum == originalnum)
-        {
-            System.out.println(originalnum +" is palindrome");
-        }
-        else {
-            System.out.println(originalnum+" is not palindrome");
-        }
+        float circum = (float) (2*Math.PI*rad);
+        System.out.println(circum);
     }
 }
